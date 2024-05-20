593536148880a2a8be72769b8ab3553e7e09b873
Delemangi <milev.stefan@gmail.com>
2024-05-20 03:10:52 +0200

    Progress

--- src/base.ts
+++ src/base.ts
@@ -1,22 +1,32 @@
 import { type Linter } from "eslint";
 import globals from "globals";
+import parser from "@typescript-eslint/parser";
+
+const rules: Linter.RulesRecord = {
+  "array-callback-return": [
+    "error",
+    {
+      allowImplicit: true,
+    },
+  ],
+};
 
 const baseConfig: Linter.FlatConfig = {
   languageOptions: {
+    parser,
+    parserOptions: {
+      project: true,
+    },
     ecmaVersion: "latest",
     sourceType: "module",
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
     globals: {
       ...globals.es2021,
       ...globals.node,
       ...globals.browser,
     },
   },
-  files: ['**/*.{js,jsx,cjs,mjs,ts,tsx}'],
+  rules,
+  files: ["**/*.{js,ts}"],
 };
 
 export default baseConfig;
