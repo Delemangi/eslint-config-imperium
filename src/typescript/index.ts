4a365a883bbbb153f6f2a99cd036d187edcacca0
Delemangi <milev.stefan@gmail.com>
2024-08-27 00:59:03 +0200

    Link TypeScript rules

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,6 +1,7 @@
 import typescriptPlugin from '@typescript-eslint/eslint-plugin';
 import typescriptParser from '@typescript-eslint/parser';
 import type { Linter } from 'eslint';
+import rules from './rules.js';
 
 const typescript: Linter.Config = {
   files: [
@@ -16,22 +17,7 @@ const typescript: Linter.Config = {
     // @ts-expect-error missing type
     '@typescript-eslint': typescriptPlugin,
   },
-  rules: {
-    'n/global-require': 'off',
-    'n/no-missing-import': 'off',
-    'no-duplicate-imports': 'off',
-    'no-undef': 'off',
-    'no-use-before-define': 'off',
-    'spaced-comment': [
-      'error',
-      'always',
-      {
-        markers: [
-          '/',
-        ],
-      },
-    ],
-  },
+  rules,
 };
 
 export default typescript;
