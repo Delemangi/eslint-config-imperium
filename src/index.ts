60120cc219c2cf173149968042ad629ff54f0c4d
Delemangi <milev.stefan@gmail.com>
2024-11-17 22:32:15 +0100

    Rename style config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import type { Linter } from 'eslint';
 
 import base from './base/index.js';
-import stylistic from './style/index.js';
+import style from './style/index.js';
 import typescript from './typescript/index.js';
 
 export const auto: Linter.Config[] = [
   base,
   typescript,
-  ...stylistic,
+  ...style,
 ];
 
 export { default as base } from './base/index.js';
 export { default as node } from './node/index.js';
-export { default as stylistic } from './style/index.js';
+export { default as style } from './style/index.js';
 export { default as typescript } from './typescript/index.js';
