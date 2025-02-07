33d1543ca889cdaca86f4ecfef88ab8e1464517f
Delemangi <milev.stefan@gmail.com>
2025-02-07 01:47:49 +0100

    chore(deps): add release information to main branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 export default {
-  branches: ["release"],
+  branches: ["main", "release"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
