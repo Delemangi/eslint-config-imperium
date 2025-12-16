0429beec5e43e244ef5ae5dd02605d86d5f40856
Delemangi <milev.stefan@gmail.com>
2025-12-16 22:16:42 +0100

    feat(typescript): support new rules

--- src/typescript/rules.ts
+++ src/typescript/rules.ts
@@ -195,6 +195,7 @@ const typescriptEslintRules = {
     }
   ],
   '@typescript-eslint/no-useless-constructor': ['error'],
+  '@typescript-eslint/no-useless-default-assignment': ['error'],
   '@typescript-eslint/no-useless-empty-export': ['error'],
   '@typescript-eslint/no-wrapper-object-types': ['error'],
   '@typescript-eslint/non-nullable-type-assertion-style': ['error'],
