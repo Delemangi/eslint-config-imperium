cd8bc31e95c0ad02fba357433196558a82fb1bfe
Delemangi <milev.stefan@gmail.com>
2026-01-06 21:22:20 +0100

    fix(perfectionist): types

--- src/perfectionist/index.ts
+++ src/perfectionist/index.ts
@@ -2,7 +2,6 @@ import type { Linter } from 'eslint';
 
 import perfectionistPlugin from 'eslint-plugin-perfectionist';
 
-// @ts-expect-error Missing types
-const perfectionist: Linter.Config = perfectionistPlugin.configs?.['recommended-natural'];
+const perfectionist: Linter.Config = perfectionistPlugin.configs['recommended-natural'];
 
 export default perfectionist;
